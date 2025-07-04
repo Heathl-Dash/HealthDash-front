cefce5b4ae5fe338f082f0ba49af9b36e2318d92
Gabriella-Aquino <gabyyaquino8@gmail.com>
2025-07-04 14:43:04 -0300

    feature: adicione componente ButtonAddBottle

---
+++ app/components/WaterModal/ButtonAddBottle/index.tsx
@@ -0,0 +1,25 @@
+import { Ionicons } from "@expo/vector-icons";
+import React from "react";
+import { StyleSheet, TouchableOpacity } from "react-native";
+
+const ButtonAddBottle = () => {
+  return (
+    <TouchableOpacity style={styles.container}>
+      <Ionicons name="add" color="white" size={45} />
+    </TouchableOpacity>
+  );
+};
+
+export default ButtonAddBottle;
+
+const styles = StyleSheet.create({
+  container: {
+    borderWidth: 1,
+    borderColor: "white",
+    paddingHorizontal: 10,
+    paddingVertical: 30,
+    borderRadius: 20,
+    justifyContent: "center",
+    alignItems: "center",
+  },
+});
