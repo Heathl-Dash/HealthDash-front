161100e3c7ff2abfc132f02ac5377c22e8ece2b1
Gabriella-Aquino <gabyyaquino8@gmail.com>
2025-07-09 16:42:55 -0300

    refactor: move a logica de index para useWater

--- app/(tabs)/index.tsx
+++ app/(tabs)/index.tsx
@@ -1,64 +1,21 @@
-import { useQuery, useQueryClient } from "@tanstack/react-query";
-import { useState } from "react";
-import { Alert, StyleSheet, Text, View } from "react-native";
+import useWater from "@/hooks/useWater";
+import { StyleSheet, View } from "react-native";
 import { SafeAreaView } from "react-native-safe-area-context";
+import AddBottleModal from "../components/AddBottleModal";
 import WaterButton from "../components/WaterButton";
 import WaterModal from "../components/WaterModal";
-import { editWaterGoal, getWaterGoal } from "../lib/axios";
-import AddBottleModal from "../components/AddBottleModal";
-
 
 export default function HomeScreen() {
-  const [waterModalIsOpen, setWaterModalIsOpen] = useState(false);
-  const [addBottleModalIsOpen, setAddBottleModalIsOpen] = useState(false);
-
-  const { data: waterGoal, error, refetch} = useQuery({
-    queryKey: ["waterGoal"],
-    queryFn: getWaterGoal,
-  });
-
-
-  const queryClient = useQueryClient();
-
-  const openWaterModal = async () => {
-    setWaterModalIsOpen(true);
-    await queryClient.refetchQueries({
-      queryKey: ["waterGoal"],
-    });
-    
-    if(error){
-      Alert.alert('Ops, aconteceu algum erro :(')
-    }
-  };
-
-  const updateWaterGoalWithBottle = async(bottle: IBottle) => {
-    if (!waterGoal) return
-
-    const newMlDrinked = waterGoal.ml_drinked + bottle.ml_bottle
-    
-    try {
-      await editWaterGoal({
-        ml_drinked: newMlDrinked,
-      });
-
-      await queryClient.invalidateQueries({
-        queryKey: ["waterGoal"],
-      });
-    } catch (err) {
-      console.error("Erro ao atualizar ml_drinked:", err);
-    }
-  };
-
-  const handleCloseWaterModal = () => {
-    setWaterModalIsOpen(false);
-  };
-  const handleCloseAddBottleModal = () => {
-    setAddBottleModalIsOpen(false);
-  };
-
-  const openAddBottle = () =>{
-    setAddBottleModalIsOpen(true)
-  }
+  const {
+    handleCloseAddBottleModal,
+    handleCloseWaterModal,
+    openAddBottle,
+    openWaterModal,
+    updateWaterGoalWithBottle,
+    waterGoal,
+    waterModalIsOpen,
+    addBottleModalIsOpen,
+  } = useWater();
 
   return (
     <SafeAreaView style={styles.container}>
@@ -80,11 +37,7 @@ export default function HomeScreen() {
         onPressBottleButton={updateWaterGoalWithBottle}
         onPressAddBottle={openAddBottle}
       />
-      <AddBottleModal
-        onClose={handleCloseAddBottleModal}
-        visible={addBottleModalIsOpen}
-      />
-
+      <AddBottleModal onClose={handleCloseAddBottleModal} visible={addBottleModalIsOpen} />
     </SafeAreaView>
   );
 }
