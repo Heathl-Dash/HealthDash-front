109f81f764267aa4625b85cdee585e25dfc9c0ac
Gabriella-Aquino <gabyyaquino8@gmail.com>
2025-07-11 20:36:49 -0300

    style: muda o icone negativo para a direita

--- components/Habit/index.tsx
+++ components/Habit/index.tsx
@@ -13,20 +13,16 @@ interface HabitProps {
 const Habit = ({ habit, onPressNegative, onPressPositive, onPressEdit }: HabitProps) => {
   return (
     <View style={styles.container}>
-      {habit.positive ? (
+      {habit.positive && (
         <TouchableOpacity style={styles.actionButton} onPress={onPressPositive}>
           <MaterialIcons size={30} color={Colors.light.primary} name="add" />
         </TouchableOpacity>
-      ) : (
-        <TouchableOpacity style={styles.actionButton} onPress={onPressNegative}>
-          <MaterialIcons size={30} color={Colors.light.redColor} name="remove" />
-        </TouchableOpacity>
-      )}
-      <TouchableOpacity style={styles.content} onPress={onPressEdit}>
+      ) }
+      <TouchableOpacity style={[styles.content, habit.negative && !habit.positive && {paddingLeft:75}]} onPress={onPressEdit}>
         <Text style={styles.title}>{habit.title}</Text>
         {habit.description && <Text style={styles.description}>{habit.description}</Text>}
       </TouchableOpacity>
-      {habit.negative && habit.positive && (
+      {habit.negative && (
         <TouchableOpacity style={styles.actionButton} onPress={onPressNegative}>
           <MaterialIcons size={30} color={Colors.light.redColor} name="remove" />
         </TouchableOpacity>
@@ -46,6 +42,7 @@ const styles = StyleSheet.create({
   content: {
     flex: 1,
     padding: 8,
+    paddingHorizontal:10
   },
   actionButton: {
     justifyContent: "center",
