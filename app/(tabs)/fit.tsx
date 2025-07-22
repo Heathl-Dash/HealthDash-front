0c8b802bce6e74fb9fa8dfda22e852e5de50687b
Gabriella-Aquino <gabyyaquino8@gmail.com>
2025-07-22 15:53:42 -0300

    feature: adiciona contagem de passos

--- app/(tabs)/fit.tsx
+++ app/(tabs)/fit.tsx
@@ -3,11 +3,54 @@ import StepCounter from "@/components/StepCounter";
 import Tabs from "@/components/Tabs";
 import { Colors } from "@/constants/Colors";
 import useHabit from "@/hooks/useHabit";
-import React, { useState } from "react";
-import { ActivityIndicator, StyleSheet, Text, TouchableOpacity, View } from "react-native";
+import React from "react";
+import { ActivityIndicator, Platform, StyleSheet, Text, View } from "react-native";
 import { FlatList } from "react-native-gesture-handler";
 import { SafeAreaView } from "react-native-safe-area-context";
 import Header from "../../components/Header";
+import {
+  isSensorWorking,
+  isStepCountingSupported,
+  parseStepData,
+  startStepCounterUpdate,
+  stopStepCounterUpdate,
+  type ParsedStepCountData,
+} from '@dongminyu/react-native-step-counter';
+import {
+  getBodySensorPermission,
+  getStepCounterPermission,
+} from '@/hooks/permission';
+
+
+ type SensorType<T = typeof Platform.OS> = T extends "ios"
+    ? "CMPedometer"
+    : T extends "android"
+      ? "Step Counter" | "Accelerometer"
+      : "NONE";
+
+  type SensorName = SensorType<Platform["OS"]>;
+
+  /** Setting the initial state of the additionalInfo object. */
+  const initState = {
+    dailyGoal: "0/10000 steps",
+    stepsString: "0 steps",
+    calories: "0 kCal",
+    distance: "0.0 m",
+  };
+
+  type AdditionalInfo = Partial<ParsedStepCountData>;
+
+  /**
+   * @returns {React.ReactComponentElement} - Returns Application Component.
+   * @description This module represents the root component of the app.
+   * 1. It imports the necessary components and libraries.
+   * 2. It defines the initial state of the additionalInfo state.
+   * 3. It defines the functions that will be used in the app.
+   * 4. It uses the useState hook to define the states that will be used in the app.
+   * 5. It uses the useEffect hook to run the isPedometerSupported function when the component mounts.
+   * 6. It uses the useEffect hook to call the startStepCounter function when the component mounts.
+   * 7. It returns the JSX code for the app.
+   */
 
 export default function Fit() {
   const {
@@ -21,20 +64,105 @@ export default function Fit() {
     addFitNegativeCounterMutation,
   } = useHabit();
 
-  const [steps, setSteps] = useState(3247);
-  const [goal] = useState(10000);
+  // const [steps, setSteps] = useState(3247);
+  // const [goal] = useState(10000);
 
+  const [loaded, setLoaded] = React.useState(false);
+  const [supported, setSupported] = React.useState(false);
+  const [granted, setGranted] = React.useState(false);
+  const [sensorType, setSensorType] = React.useState<SensorName>('NONE');
+  const [stepCount, setStepCount] = React.useState(0);
+  const [additionalInfo, setAdditionalInfo] =
+    React.useState<AdditionalInfo>(initState);
+
+  /**
+   * Get user's motion permission and check pedometer is available.
+   * This function checks if the step counting is supported by the device
+   * and if the user has granted the app the permission to use it.
+   * It sets the state variables 'granted' and 'supported' accordingly.
+   */
+  const isPedometerSupported = () => {
+    isStepCountingSupported().then((result) => {
+      setGranted(result.granted === true);
+      setSupported(result.supported === true);
+    });
+  };
+
+  /**
+   * It starts the step counter and sets the sensor type, step count, and additional info.
+   * The function startStepCounter is called when the user clicks the "Start" button.
+   * It starts the step counter.
+   */
+  const startStepCounter = () => {
+    startStepCounterUpdate(new Date(), (data) => {
+      setSensorType(data.counterType as SensorName);
+      const parsedData = parseStepData(data);
+      setStepCount(parsedData.steps);
+      setAdditionalInfo({
+        ...parsedData,
+      });
+    });
+    setLoaded(true);
+  };
+
+  /**
+   * It sets the state of the additionalInfo object to its initial state, stops the step counter update,
+   * and sets the loaded state to false.
+   * This function is used to stop the step counter.
+   */
+  const stopStepCounter = () => {
+    setAdditionalInfo(initState);
+    stopStepCounterUpdate();
+    setLoaded(false);
+  };
+
+  /**
+   * If the sensor is working, stop it. If it's not working,
+   * Get permission for the other sensor and start it.
+   * This function is used to force the use of another sensor.
+   */
+  const forceUseAnotherSensor = () => {
+    if (isSensorWorking) {
+      stopStepCounter();
+    } else {
+      if (sensorType === 'Step Counter') {
+        getBodySensorPermission().then(setGranted);
+      } else {
+        getStepCounterPermission().then(setGranted);
+      }
+    }
+    startStepCounter();
+  };
+
+  /**
+   * A hook that runs when the component mounts. It calls the isPedometerSupported function
+   * and returns a function that stops the step counter.
+   * This effect runs when the component is first mounted
+   * and then runs again when the `count` variable changes.
+   */
+  React.useEffect(() => {
+    isPedometerSupported();
+    return () => {
+      stopStepCounter();
+    };
+  }, []);
+
+  /**
+   * A hook that runs when the component mounts.
+   * It calls the isPedometerSupported function and returns a
+   * function that stops the step counter.
+   */
+  React.useEffect(() => {
+    console.debug(`🚀 stepCounter ${supported ? '' : 'not'} supported`);
+    console.debug(`🚀 user ${granted ? 'granted' : 'denied'} stepCounter`);
+    startStepCounter();
+  }, [granted, supported]);
 
   return (
     <SafeAreaView style={{ flex: 1, paddingHorizontal: 30, flexGrow: 1 }}>
       <Header />
-      <View style={{justifyContent: "center", alignItems: "center", marginTop: 20}}>
-        <StepCounter
-          steps={steps}
-          goal={goal}
-          size={250}
-          strokeWidth={15}
-        />
+      <View style={{ justifyContent: "center", alignItems: "center", marginTop: 20 }}>
+        <StepCounter steps={stepCount} goal={1050} size={250} strokeWidth={15} />
       </View>
 
       <View style={{ marginTop: 35, marginBottom: 25 }}>
