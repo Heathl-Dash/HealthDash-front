cb9937baeff8594cfdcccaa1b0e81ca91ff4a4f0
Gabriella-Aquino <gabyyaquino8@gmail.com>
2025-12-17 18:22:09 -0300

    feat: começa componente de Attach

---
+++ components/Attach/index.tsx
@@ -0,0 +1,80 @@
+import { Colors } from "@/constants/Colors";
+import React from "react";
+import { StyleSheet, Text, View } from "react-native";
+
+interface AttachProps {
+  type: "toDo" | "habit";
+  attach: IAttach | null;
+}
+
+const Attach = ({ type, attach }: AttachProps) => {
+  return (
+    <View style={styles.container}>
+      <View style={styles.textContent}>
+        <Text style={styles.title}>{attach?.title}</Text>
+        {attach?.description && <Text style={styles.description}>{attach.description}</Text>}
+        <View style={styles.counterContainer}></View>
+      </View>
+      <View style={styles.counterContainer}>
+        {attach?.isNegative && (
+          <View style={[styles.counterContent, { backgroundColor: "#67B8F9" }]}>
+            <Text style={styles.counterText}>-{attach.negativeCount?.toString()}</Text>
+          </View>
+        )}
+        {attach?.isPositive && (
+          <View style={[styles.counterContent, { backgroundColor: "#4FACF7" }]}>
+            <Text style={styles.counterText}>+{attach.positiveCount?.toString()}</Text>
+          </View>
+        )}
+      </View>
+    </View>
+  );
+};
+
+export default Attach;
+
+const styles = StyleSheet.create({
+  container: {
+    width: "100%",
+    gap: 10,
+    padding: 20,
+    paddingTop: 25,
+    borderRadius: 25,
+    backgroundColor: Colors.light.primary,
+  },
+  textContent: {
+    width: "100%",
+    justifyContent: "center",
+    alignItems: "center",
+  },
+  title: {
+    color: "white",
+    fontWeight: "700",
+  },
+  description: {
+    paddingTop: 10,
+    color: "white",
+    opacity: 9,
+    textAlign: 'center',
+  },
+  typeIdentifier: {
+    color: "white",
+    opacity: 8.5,
+  },
+  counterContainer: {
+    width: "100%",
+    flexDirection: "row",
+    gap: 15,
+    justifyContent: "flex-end",
+  },
+  counterContent: {
+    width: 50,
+    aspectRatio: 1,
+    borderRadius: 50,
+    justifyContent: "center",
+    alignItems: "center",
+  },
+  counterText: {
+    color: "white",
+  },
+});
---
+++ types/attach.d.ts
@@ -0,0 +1,9 @@
+declare interface IAttach {
+  title: string;
+  description?: string;
+  done?: boolean;
+  isPositive?: boolean;
+  isNegative?: boolean;
+  positiveCount?: number
+  negativeCount?: number
+}
