5ec7debe224a099b565dc3310e5847f51fa0cea6
Gabriella-Aquino <gabyyaquino8@gmail.com>
2025-12-27 14:44:29 -0300

    feat: adiciona visual para as ações curtir e comentar

--- components/Publication/index.tsx
+++ components/Publication/index.tsx
@@ -1,6 +1,7 @@
 import { Colors } from "@/constants/Colors";
+import { FontAwesome, MaterialCommunityIcons } from "@expo/vector-icons";
 import React from "react";
-import { StyleSheet, Text, View } from "react-native";
+import { StyleSheet, Text, TouchableOpacity, View } from "react-native";
 import Attach from "../Attach";
 
 interface PublicationProps {
@@ -11,6 +12,9 @@ interface PublicationProps {
   isPublic?: boolean;
   type?: "normal" | "toDo" | "habit";
   attach?: IAttach | null;
+  isLike?: boolean;
+  likesCount: number;
+  commentsCount: number;
 }
 
 const Publication = ({
@@ -21,6 +25,9 @@ const Publication = ({
   description,
   images,
   title,
+  isLike,
+  likesCount,
+  commentsCount,
 }: PublicationProps) => {
   return (
     <View style={styles.container}>
@@ -33,8 +40,24 @@ const Publication = ({
           <Text>{description}</Text>
         </View>
       )}
+
       {attach && type != "normal" && <Attach attach={attach} type={type} />}
       {images && <View>imagens</View>}
+
+      <View style={styles.actionsContainer}>
+        <TouchableOpacity style={styles.action}>
+          {isLike ? (
+            <MaterialCommunityIcons size={30} color={Colors.light.darkGray} name="heart" />
+          ) : (
+            <MaterialCommunityIcons size={30} color={Colors.light.darkGray} name="heart-outline" />
+          )}
+          <Text>{likesCount}</Text>
+        </TouchableOpacity>
+        <TouchableOpacity style={styles.action}>
+          <FontAwesome name="comment-o" size={28} color={Colors.light.darkGray}/> <Text>{commentsCount}</Text>
+        </TouchableOpacity>
+      </View>
+      <View style={{width: '100%', borderTopColor: Colors.light.lightGray, borderTopWidth: 1}}/>
     </View>
   );
 };
@@ -56,5 +79,15 @@ const styles = StyleSheet.create({
     height: 40,
     borderRadius: 50,
     backgroundColor: Colors.light.primary,
+  },
+  actionsContainer: {
+    flexDirection: "row",
+    gap: 10,
+    alignItems: "center",
+  },
+  action: {
+    flexDirection: "row",
+    alignItems: "center",
+    gap: 3,
   },
 });
--- types/publication.d.ts
+++ types/publication.d.ts
@@ -1,7 +1,11 @@
-declare interface IPublication{
+declare interface IPublication {
+  id:number;
   description: string;
-    profileId: number;
-    isPublic: boolean;
-    type: "toDo" | "habit" | "normal";
-    attach: IAttach
+  profileId: number;
+  isPublic: boolean;
+  type: "toDo" | "habit" | "normal";
+  attach: IAttach;
+  isLike?: boolean;
+  likesCount: number;
+  commentsCount: number
 }
