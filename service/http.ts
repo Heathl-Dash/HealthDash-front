1111a25a9ada65217b160f613c07d6256e04d6b7
Gabriella-Aquino <gabyyaquino8@gmail.com>
2026-01-31 14:11:50 -0300

    fix: corrige refresh

--- service/http.ts
+++ service/http.ts
@@ -9,7 +9,7 @@ export const createApi = (baseURL: string) => {
       const tokens = await storage.getTokens();
 
       // console.log("tokens", tokens);
-      console.log(config.url)
+      console.log(config.url);
 
       if (tokens?.access) {
         config.headers.Authorization = `Bearer ${tokens.access}`;
@@ -20,64 +20,52 @@ export const createApi = (baseURL: string) => {
     (error) => Promise.reject(error)
   );
 
-  // api.interceptors.response.use(
-  //   (response) => response,
-  //   async (error) => {
-  //     if (error.response) {
-  //       // The server responded with a status code outside the 2xx range
-  //       // console.log(error.response.data);
-  //       // console.log(error.response.status);
-  //     } else if (error.request) {
-  //       // The request was made but no response was received (e.g., network error, server down)
-  //       // console.log(error.request);
-  //     } else {
-  //       // Something happened in setting up the request that triggered an Error
-  //       // console.log("Error", error.message);
-  //     }
+  api.interceptors.response.use(
+    (response) => response,
+    async (error) => {
+      const originalRequest = error.config;
 
-  //     const originalRequest = error.config;
+      if (
+        (error.response?.status === 401 || error.response?.status === 403) &&
+        !originalRequest._retry
+      ) {
+        originalRequest._retry = true;
 
-  //     if (
-  //       (error.response?.status === 401 || error.response?.status === 403) &&
-  //       !originalRequest._retry
-  //     ) {
-  //       originalRequest._retry = true;
+        const refresh = await storage.getRefreshToken();
 
-  //       // console.log("tentando refresh");
+        if (!refresh) {
+          await storage.removeAccessToken();
+          await storage.removeRefreshToken();
+          return Promise.reject(error);
+        }
 
-  //       const refresh = await storage.getRefreshToken();
+        try {
+          const response = await Axios.post(
+            `${process.env.EXPO_PUBLIC_PROFILE_API}/profiles/token/refresh/`,
+            { DashboardProfileRefresh: refresh }
+          );
 
-  //       if (!refresh) {
-  //         await storage.removeAccessToken();
-  //         await storage.removeRefreshToken();
-  //         return Promise.reject(error);
-  //       }
+          const newAccess = response.data.DashboardProfileAccess;
+          const newRefresh = response.data.DashboardProfileRefresh;
 
-  //       try {
-  //         const response = await Axios.post(
-  //           `${process.env.EXPO_PUBLIC_PROFILE_API}/profiles/token/refresh/`,
-  //           { DashboardProfileRefresh: refresh }
-  //         );
+          await storage.saveTokens({
+            DashboardProfileAccess: newAccess,
+            DashboardProfileRefresh: newRefresh,
+          });
 
-  //         const newAccess = response.data.DashboardProfileAccess;
-  //         const newRefresh = response.data.DashboardProfileRefresh;
+          originalRequest.headers.Authorization = `Bearer ${newAccess}`;
 
-  //         await storage.setAccessToken(newAccess);
-  //         await storage.setRefreshToken(newRefresh);
+          return api(originalRequest);
+        } catch (err) {
+          await storage.removeAccessToken();
+          await storage.removeRefreshToken();
+          return Promise.reject(err);
+        }
+      }
 
-  //         originalRequest.headers.Authorization = `Bearer ${newAccess}`;
-
-  //         return api(originalRequest);
-  //       } catch (err) {
-  //         await storage.removeAccessToken();
-  //         await storage.removeRefreshToken();
-  //         return Promise.reject(err);
-  //       }
-  //     }
-
-  //     return Promise.reject(error);
-  //   }
-  // );
+      return Promise.reject(error);
+    }
+  );
 
   return api;
 };
